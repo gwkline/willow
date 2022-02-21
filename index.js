a09429850d8854b6c03ce041a85abf69a80c888c
Gavin Kline <gwkline23@gmail.com>
2022-02-21 17:27:56 -0500

    Cleaned stuff up

--- index.js
+++ index.js
@@ -14,26 +14,5 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get('/', (req, res) => {
 
-    res.status(200).sendFile(path.join(__dirname, './discordauth/index.html'));
     res.status(200).sendFile(path.join(__dirname, 'index.html'));
-    //loginindex.html
 });
-
-
-// // Routes
-// app.use('/api/discord', require('.discordauth/api/discord.js'));
-
-// app.use((err, req, res, next) => {
-//     switch (err.message) {
-//         case 'NoCodeProvided':
-//             return res.status(400).send({
-//                 status: 'ERROR',
-//                 error: err.message,
-//             });
-//         default:
-//             return res.status(500).send({
-//                 status: 'ERROR',
-//                 error: err.message,
-//             });
-//     }
-// });
