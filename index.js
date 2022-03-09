a0f7463fce0f7e40a585790bb5cb9f8a0279f0a6
Gavin Kline <gwkline23@gmail.com>
2022-03-09 17:57:21 -0500

    Fixing react dash

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 50000
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,8 +12,8 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 app.get('/', (req, res) => {
-
-    res.status(200).sendFile(path.join(__dirname, './react/app.js'));
+    res.type('.js');
+    res.status(200).sendFile(path.join(__dirname, './src/index.js'));
 });
 
 app.get('/login', (req, res) => {
