8d46069b1edd44c1cf1824e0141de8c09c185544
Gavin Kline <gwkline23@gmail.com>
2022-03-25 10:46:59 -0400

    fixed rendering

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 app.get('/', (req, res) => {
-    res.type('.js');
-    res.status(200).sendFile(path.join(__dirname, './src/Dash/index.html'));
+    res.sendFile(path.join(__dirname, './src/Dash/index.html'));
 });
 
 app.get('/login', (req, res) => {
