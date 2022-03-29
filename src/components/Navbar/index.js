1561e6e75ba56da350fa8d18073899428c216758
Gavin Kline <gwkline23@gmail.com>
2022-03-29 18:34:31 -0400

    fixed import

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Nav, NavLink, NavMenu, NavTitle } 
     from "./NavbarElements";
 import { useAuthState } from "react-firebase-hooks/auth";
-import auth from "../Login/firebase"
+import { auth } from "../Login/firebase"
 import leaf from "../../images/leaf.png";
   
 const Navbar = () => {
