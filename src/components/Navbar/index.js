08d4e4cc683d0624eadb647fc295bb05bb0eb9de
Gavin Kline <gwkline23@gmail.com>
2022-03-29 15:58:05 -0400

    Final idk

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  auth,
+  registerWithEmailAndPassword,
+  signInWithGoogle,
+} from "../Login/firebase";
 import { Nav, NavLink, NavMenu } 
     from "./NavbarElements";
   
+  
 const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <NavMenu>
-          <NavLink to="/">
-            Home
-          </NavLink>
-          <NavLink to="/createproject">
-            Create Project
-          </NavLink>
-          <NavLink to="/projects">
-            Projects
-          </NavLink>
-          <NavLink to="/Updates">
-            Updates
-          </NavLink>
-          <NavLink to="/settings">
-            Settings
-          </NavLink>
-          <NavLink to="/login">
-            Login
-          </NavLink>
-          <NavLink to="/register">
-            Register
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
-  );
+  const [user, loading, error] = useAuthState(auth);
+  if (user) {
+    return (
+      <>
+        <Nav>
+          <NavMenu>
+            <NavLink to="/">
+              Home
+            </NavLink>
+            <NavLink to="/createproject">
+              Create Project
+            </NavLink>
+            <NavLink to="/projects">
+              Projects
+            </NavLink>
+            <NavLink to="/Updates">
+              Updates
+            </NavLink>
+            <NavLink to="/settings">
+              Settings
+            </NavLink>
+            <NavLink to="/dashboard">
+              Dashboard
+            </NavLink>
+          </NavMenu>
+        </Nav>
+      </>
+    );
+  }
+  else {
+    return (
+      <>
+        <Nav>
+          <NavMenu>
+            <NavLink to="/">
+              Home
+            </NavLink>
+            <NavLink to="/createproject">
+              Create Project
+            </NavLink>
+            <NavLink to="/projects">
+              Projects
+            </NavLink>
+            <NavLink to="/Updates">
+              Updates
+            </NavLink>
+            <NavLink to="/settings">
+              Settings
+            </NavLink>
+            <NavLink to="/login">
+              Login
+            </NavLink>
+            <NavLink to="/register">
+              Register
+            </NavLink>
+          </NavMenu>
+        </Nav>
+      </>
+    );
+  }
+
+
+  
 };
   
 export default Navbar;
