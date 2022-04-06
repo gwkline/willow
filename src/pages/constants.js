6ac1c7f160744bd792b0c909daa6ca2b382385da
lghannah <lghannah@syr.edu>
2022-04-05 23:41:26 -0400

    roles and perms

--- src/pages/constants.js
+++ src/pages/constants.js
@@ -1,11 +1,16 @@
-import Projects from ./projects.js;
+import Projects from "./projects.js";
 
 
 const actions = {
     MODIFY_FILE: "MODIFY_FILE",
     VIEW_FILE: "VIEW_FILE",
     DELETE_FILE: "DELETE_FILE",
-    CREATE_FILE: "CREATE_FILE"
+    CREATE_FILE: "CREATE_FILE",
+    ADD_USER: "ADD_USER",
+    REMOVE_USER: "REMOVE_USER",
+    MANAGE_ROLES: "MANAGE_ROLES",
+    ASSIGN_TASKS: "ASSIGN_TASKS",
+    CHANGE_STATUS: "CHANGE_STATUS"
   };
   
   const roles = {
@@ -15,20 +20,20 @@ const actions = {
   
   export { actions, roles };
 
-const roles.GROUPLEAD = {
+roles.GROUPLEAD = {
   id: 1,
   title: user.Projects,
   authorId: user?.UID,
-  accessLevel: "Group Leader",
-  content: {...}
+  accessLevel: "Group Leader"
+  //content: {...}
 };
 
-const roles.GROUPMEMBER = {
+roles.GROUPMEMBER = {
     id: 2,
     title: user.Projects,
     authorID: user?.UID, 
-    accessLevel: "Group Member",
-    content: {...}
+    accessLevel: "Group Member"
+    //content: {...}
 };
 
 export { actions, roles };
--- src/pages/permissions.js
+++ src/pages/permissions.js
@@ -6,3 +6,8 @@ mappings.set(actions.MODIFY_FILE, [roles.GROUPLEAD, roles.GROUPMEMBER]);
 mappings.set(actions.VIEW_FILE, [roles.GROUPLEAD, roles.GROUPMEMBER]);
 mappings.set(actions.DELETE_FILE, [roles.GROUPLEAD]);
 mappings.set(actions.CREATE_FILE, [roles.GROUPLEAD, roles.GROUPMEMBER]);
+mappings.set(actions.ADD_USER, [roles.GROUPLEAD]);
+mappings.set(actions.REMOVE_USER, [roles.GROUPLEAD]);
+mappings.set(actions.MANAGE_ROLES, [roles.GROUPLEAD]);
+mappings.set(actions.ASSIGN_TASKS, [roles.GROUPLEAD]);
+mappings.set(actions.CHANGE_STATUS, [roles.GROUPLEAD, roles.GROUPMEMBER]);
